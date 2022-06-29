7a06011d20723d6d886df6756614794d04f15f03
zhaoxy <1007977461@qq.com>
2022-06-29 15:18:11 +0800

    test1

---
+++ common/service_utils/src/main/java/com/yygh/common/gitdemo/gitdemo.java
@@ -0,0 +1,13 @@
+package com.yygh.common.gitdemo;
+
+/**
+ * @ClassName : gitdemo  //类名
+ * @Description : 测试git  //描述
+ * @Author : 10079 //作者
+ * @Date: 2022/6/28  17:48
+ */
+public class gitdemo {
+    public static void main(String[] args) {
+        
+    }
+}
